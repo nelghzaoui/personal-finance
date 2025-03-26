1345ae6230720a44f9eb56e16c81a2ad87dd463c
Nasreddine <eln@outlook.be>
2025-03-26 12:06:44 +0100

    add destroy

--- src/app/shared/components/button.component.ts
+++ src/app/shared/components/button.component.ts
@@ -7,13 +7,14 @@ import { Component, input } from '@angular/core';
   template: `
     <button
       type="button"
-      class="text-preset-4-bold rounded-lg cursor-pointer p-4"
+      class="text-preset-4-bold rounded-lg cursor-pointer text-white p-4 transition-colors duration-200"
       [ngClass]="{
-        'bg-grey-900 text-white hover:bg-grey-500': style() === 'primary',
+        'bg-grey-900 hover:bg-grey-500': style() === 'primary',
         'bg-grey-100 text-grey-900 hover:bg-white hover:border hover:border-grey-500':
           style() === 'secondary',
         'flex justify-between p-0 gap-4 text-grey-500 font-normal hover:text-grey-900':
-          style() === 'tertiary'
+          style() === 'tertiary',
+        'bg-red hover:bg-red-300': style() === 'destroy'
       }"
     >
       {{ label() }}
@@ -29,4 +30,4 @@ export class ButtonComponent {
   style = input<Style>();
 }
 
-type Style = 'primary' | 'secondary' | 'tertiary';
+type Style = 'primary' | 'secondary' | 'tertiary' | 'destroy';
