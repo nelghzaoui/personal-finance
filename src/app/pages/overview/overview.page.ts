ee72d3646d3bb1f2d903981ad43092ef83ea5771
Nasreddine <eln@outlook.be>
2025-03-26 10:53:47 +0100

    add custom colors, fonts

--- src/app/pages/overview/overview.page.ts
+++ src/app/pages/overview/overview.page.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '',
-  template: ``
+  template: `
+    <h1 class="text-preset-1 text-gold">Titre principal (Preset 1)</h1>
+
+    <p class="text-preset-4">
+      Ceci est un paragraphe avec les specs de Preset 4 (14px, regular)
+    </p>
+
+    <p class="text-preset-5-bold">Petit texte fort, preset 5 bold</p>
+  `
 })
 export class OverviewPage implements OnInit {
   ngOnInit() {}
